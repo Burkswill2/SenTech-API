8aeabe373c0c65e9832661f439b8eeaf725b0c68
burkswill2 <burks.113@osu.edu>
2024-10-20 00:22:20 -0700

    build repairs

--- app/main.py
+++ app/main.py
@@ -1,22 +1,19 @@
 from fastapi import Depends, FastAPI
 
 # from .dependencies import get_query_token, get_token_header
-from .internal import admin
-from .routers import items, users, markowitz_model
+# from .internal import admin
+from .routers import markowitz_model
 
 # app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
-
-app.include_router(users.router)
-app.include_router(items.router)
 app.include_router(markowitz_model.router)
-app.include_router(
-    admin.router,
-    prefix="/admin",
-    tags=["admin"],
-    # dependencies=[Depends(get_token_header)],
-    responses={418: {"description": "I'm a teapot"}},
-)
+# app.include_router(
+#     admin.router,
+#     prefix="/admin",
+#     tags=["admin"],
+#     # dependencies=[Depends(get_token_header)],
+#     responses={418: {"description": "I'm a teapot"}},
+# )
 
 
 @app.get("/")
