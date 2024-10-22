007f4bf67eb23542620314f6a6f28cf9fb3302f0
burkswill2 <burks.113@osu.edu>
2024-10-22 11:14:58 -0700

    Added CORSMiddleware from FastAPI

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # from .dependencies import get_query_token, get_token_header
 # from .internal import admin
@@ -6,6 +7,20 @@ from routers import markowitz_model
 
 # app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
+
+# CORS configuration
+origins = [
+    "http://localhost:3000",  # Example: your frontend URL
+    # "https://yourdomain.com",  # Add your production URL here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Specify the allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 app.include_router(markowitz_model.router)
 # app.include_router(
 #     admin.router,
